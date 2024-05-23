0d11df63beb54c9ce832cff586703933f24d2f8b
annabelle_zhang <annabellezhang0104@gmail.com>
2024-05-23 11:23:49 -0400

    adjusted enemy spawn in lair and added a MouseClicked method

--- src/Projectile.java
+++ src/Projectile.java
@@ -1,14 +1,18 @@
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-public class Projectile {
-
+public class Projectile{
 
     private Point location;
     private int speed;
     private int maxFrames, frameCount;
 
     public Projectile(Point location) {
+        super();
         this.location = location;
+    }
+
+    public void draw2(Graphics2D g2, BufferedImage image){
+        g2.drawImage(image, location.x, location.y, null);
     }
 }
