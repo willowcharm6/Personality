f05ffec24231165eb45a7e3191cee229c4f377ac
annabelle_zhang <annabellezhang0104@gmail.com>
2024-05-23 09:37:00 -0400

    added main to git

---
+++ src/Projectile.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Projectile {
+
+
+    private Point location;
+    private int speed;
+    private int maxFrames, frameCount;
+//        super(Resources., location);
+
+    public Projectile(BufferedImage image, Point location){
+    }
+
+}
